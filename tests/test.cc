5105c4cbe41b473862f638666d8711a6daffc9db
Myungsun Kim <msunkim@Monk.local>
2020-09-11 16:03:56 +0900

    tests/test codes

--- tests/test.cc
+++ tests/test.cc
@@ -8,6 +8,7 @@
 \* ************************************************************************************************************************************ */
 
 #include <iostream>
+#include <cstdio>
 
 #include "crypt.h"
 
@@ -17,11 +18,28 @@ main(int argc, char** argv)
     // using std::cout;
     // using std::endl;
 
-    _openssl_BN a, b(100);
+    uint8_t     bytes[1000] = {0};
+    _openssl_BN a, b(100), c = 1;
+    int         len = 0;
+
+    
 
     ///< random test
-    std::cout << "1. Random value generation: " << std::endl;
-    a._randomInplace(b);
+    std::cout << "1. ## Random Generation ##" << std::endl;
+    a._randomInplace(1024);
+    std::cout << "size in bytes: " << a._getByteSize() << std::endl;
+    std::cout << "number in hex: [" << a._bn2hex() << "]" << std::endl;
+    a._bn2byte(bytes, &len);
+    std::cout << "into a binary string of size " << len << " : [";
+    for (auto i = 0; i < len; i++) {
+        std::printf("%02X", bytes[i]);
+    }
+    std::cout << "]" << std::endl << std::endl;
+
+    ///< assignment
+    std::cout << "2. ## Assignment ##" << std::endl;
+    _openssl_BN d = a;
+    std::cout << "number in hex: [" << d._bn2hex() << "]" << std::endl << std::endl;
 
     return 1;
 }
